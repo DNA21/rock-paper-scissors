3c28a41e6387b99b1d1d832e72d75f4508c96101
Daniel Galuardi <dan.galuardi@gmail.com>
2022-10-04 15:06:34 -0400

    Add game function to play 5 rounds

--- script.js
+++ script.js
@@ -17,32 +17,65 @@ function getComputerChoice(){
 // Returns choice with the first letter capitalized
 
 function getPlayerChoice(){
-    let playerChoice = 'PapER';
+    let playerChoice = prompt('Rock, Paper, or Scissors:');
     playerChoice = playerChoice.toLowerCase();
-    firstLetter = playerChoice.charAt(0).toUpperCase();
+    let firstLetter = playerChoice.charAt(0).toUpperCase();
     playerChoice = firstLetter.concat(playerChoice.slice(1));
     return playerChoice
 }
 
 // Decide winner
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection, playerWin, computerWin){
     if (playerSelection === computerSelection){
         return "Tie!"
     
     // If Player wins, return player winner else return computer winner
 
-    } else if (playerSelection === 'Rock' && 
-    computerSelection === 'Paper' || playerSelection === 'Paper'
-    && computerSelection === 'Scissors' || playerSelection === 'Scissors'
-    && computerSelection === 'Rock'){
-        return "You Won! " + playerSelection + ' beats ' + computerSelection
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper' || 
+    playerSelection === 'Paper' && computerSelection === 'Scissors' || 
+    playerSelection === 'Scissors' && computerSelection === 'Rock'){
+        playerWin += 1;
+        return "You Won! " + playerSelection + ' beats ' + computerSelection, playerWin
     } else {
-        return "You Lose! " + computerSelection + ' beats ' + playerSelection
+        computerWin += 1;
+        return "You Lose! " + computerSelection + ' beats ' + playerSelection, computerWin
     }
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
+//const playerSelection = getPlayerChoice();
+//const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection))
+//console.log(playRound(playerSelection, computerSelection))
+
+// play a five round game and keep score
+
+function game(){
+    let playerWin = 0;
+    let computerWin = 0;
+    for (let i = 0; i < 5; i++){
+        const computerSelection = getComputerChoice();
+        const playerSelection = getPlayerChoice();
+        if (playerSelection === computerSelection){
+            console.log("Tie!")
+            continue
+        
+        // If Player wins, return player winner else return computer winner
+    
+        } else if (playerSelection === 'Rock' && computerSelection === 'Paper' || 
+        playerSelection === 'Paper' && computerSelection === 'Scissors' || 
+        playerSelection === 'Scissors' && computerSelection === 'Rock'){
+            playerWin += 1;
+            console.log("You Won! " + playerSelection + ' beats ' + computerSelection)
+            continue
+        } else {
+            computerWin += 1;
+            console.log("You Lose! " + computerSelection + ' beats ' + playerSelection)
+            continue
+        }
+    }
+
+    return 'Player: ' + playerWin + ' Computer: ' + computerWin
+}
+
+console.log(game());
