643969e6619e2370b69de7cd23335ac80c2183ce
Daniel Galuardi <dan.galuardi@gmail.com>
2022-10-04 13:28:38 -0400

    Random computer starting choice

--- script.js
+++ script.js
@@ -2,6 +2,19 @@
 
 // Computer randomly chooses rock paper or scissors
 
+
+function getComputerChoice(){
+    const compChoice = Math.floor(Math.random() * 3) + 1
+    if (compChoice === 1) {
+        return "Rock"
+    } else if (compChoice === 2){
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
+
+console.log(getComputerChoice())
 // Player chooses selection
 
 // Decide winner
